8ff0152a50998f831e820eb4d7b3de8170aa13b6
yan cui <yan.cui@hulu.com>
2016-01-10 22:47:49 -0800

    convert to html5 video

--- main_desktop.cpp
+++ main_desktop.cpp
@@ -6,16 +6,19 @@
 #include "opencv2/imgcodecs/imgcodecs.hpp"
 #include "opencv2/videoio/videoio.hpp"
 
-// Include the rest of our code!
 #include "mode.h"
 
 using namespace cv;
 using namespace std;
 
-bool m_sketchMode = false, m_evilMode = false, m_detectMode = false, openedHulu = true, m_cartoonMode = true;
+bool m_sketchMode = false, m_evilMode = false, m_3dMode = false, m_cartoonMode = true;
 
 int main(int argc, char *argv[])
 {
+    if (argc != 5) {
+        cout << "usage: Hackathon <input file> <output file> <frames> <mode>" << endl;
+        return -1;
+    }
     Mat LoadedImage;
     VideoCapture cap(argv[1]);
     
@@ -23,30 +26,38 @@ int main(int argc, char *argv[])
                   (int) cap.get(CAP_PROP_FRAME_HEIGHT));
     Size SizeOfFrame = cv::Size(S.width, S.height);
     
-    VideoWriter video(argv[2], CV_FOURCC('8', 'B', 'P', 'S'), 30, SizeOfFrame, true);
-    
+    VideoWriter video("tmp.mp4", CV_FOURCC('8', 'B', 'P', 'S'), 30, SizeOfFrame, true);
 
     cout << "S=" << S << endl;
-    for (int i = 0; i<100; i++)
+    if (0 == strcmp(argv[4], "sketch"))
+        m_sketchMode = true;
+    else if (0 == strcmp(argv[4], "evil"))
+        m_evilMode = true;
+    else if (0 == strcmp(argv[4], "3d"))
+        m_3dMode = true;
+    else m_cartoonMode = true;
+    cout << argv[4] << " mode" << endl;
+    
+    for (int i = 0; i<stoi(argv[3]); i++)
     {
-        
         bool Is = cap.grab();
         if (Is == false) {
-            
             cout << "cannot grab video frame" << endl;
-            
         }
         else {
-            
-            // Receive video from your source
             cap.retrieve(LoadedImage, CV_CAP_OPENNI_BGR_IMAGE);
             
             resize(LoadedImage, LoadedImage, Size(S.width, S.height));
                 
-            cout << "Saving video" << endl;
+            cout << i << "th frame" << endl;
             Mat displayedFrame = Mat(LoadedImage.size(), CV_8UC3);
-            playLiveOnMode(LoadedImage, displayedFrame, m_sketchMode, m_evilMode, m_detectMode, m_cartoonMode);
+            playLiveOnMode(LoadedImage, displayedFrame, m_sketchMode, m_evilMode, m_3dMode, m_cartoonMode);
             video.write(displayedFrame);
         }
     }
+    string cmd = "ffmpeg -i tmp.mp4 -qscale 4 -vcodec libx264 -f mp4 " + string(argv[2]);
+    system(cmd.c_str());
+    system("rm tmp.mp4");
+    
+    return 0;
 }
